ed373cfd3b43b63bb2390ed0a9b44c1205b2ead1
Juan Flores <juanantoniovicaria@gmail.com>
2016-07-06 07:36:39 +0100

    Add files via upload

---
+++ UserController.js
@@ -0,0 +1,30 @@
+(function() {
+
+  var app = angular.module("githubViewer");
+  
+  var UserController = function($scope, github, $routeParams) {
+
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user)
+            .then(onRepos, onError);
+    };
+    
+    var onRepos = function(response){
+      $scope.repos = response;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+    $scope.username = $routeParams.username;
+    $scope.repoSortOrder = "-stargazers_count";
+    github.getUser($scope.username)
+    .then(onUserComplete, onError);
+  };
+  
+  app.controller("UserController", 
+  ["$scope", "github", "$routeParams", 
+  UserController]);
+}());
